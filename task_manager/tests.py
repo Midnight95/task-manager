bfcda8aa02aebeb9a61e6cf031e5e86375915ba8
Midnight <ivanator666@gmail.com>
2024-02-05 13:38:55 +0300

    updated test

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -4,18 +4,19 @@ from task_manager.user.models import User
 
 
 class TestMixin(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.credentials = {
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.credentials = {
             'username': 'user',
             'password': 'ASFD&l098877'
         }
 
-        self.user = User.objects.create(**self.credentials)
+        cls.user = User.objects.create(**cls.credentials)
 
 
 class TestIndex(TestMixin):
     def test_index_view(self):
         response = self.client.get(reverse_lazy('home'))
 
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200) 
