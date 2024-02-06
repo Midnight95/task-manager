f855bfc7e21b2ed4c991d589fc94941bfba64998
Midnight <ivanator666@gmail.com>
2024-02-06 17:25:37 +0300

    updated tests

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -21,10 +21,10 @@ class TestIndex(TestCase):
 class TestUser(TestCase):
     def setUp(self):
         self.data = {
-            'username': 'user',
             'email': 'test@email.com',
             'first_name': 'User',
             'last_name': 'Uservocich',
+            'username': 'user',
             'password1': 'jgoi34^@dFF',
             'password2': 'jgoi34^@dFF'
             }
@@ -33,3 +33,18 @@ class TestUser(TestCase):
         response = self.client.get(reverse_lazy('user_create'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='user/user_form.html')
+
+    def test_user_creation(self):
+        response = self.client.post(reverse_lazy('user_create'), data=self.data)
+        self.assertRedirects(response, expected_url=reverse_lazy('login'))
+        
+        user = User.objects.get(username="user")
+        self.assertTrue(user.is_active)
+
+    def test_user_login_url(self):
+        response = self.client.get(reverse_lazy('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='user/user_form.html')
+
+    def test_user_login(self):
+        
