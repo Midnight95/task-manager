39a138f1d139405a4309948e3836b386ff66cbbe
Midnight <ivanator666@gmail.com>
2024-02-07 17:46:58 +0300

    updated test

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -18,7 +18,7 @@ class TestIndex(TestCase):
         self.assertNotContains(self.response, '/logout/')
 
 
-class TestUser(TestCase):
+class TestUserCreation(TestCase):
     def setUp(self):
         self.data = {
             'email': 'test@email.com',
@@ -29,7 +29,7 @@ class TestUser(TestCase):
             'password2': 'jgoi34^@dFF'
             }
 
-    def test_user_url(self):
+    def test_user_creation_url(self):
         response = self.client.get(reverse_lazy('user_create'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='user/user_form.html')
@@ -41,10 +41,33 @@ class TestUser(TestCase):
         user = User.objects.get(username="user")
         self.assertTrue(user.is_active)
 
+
+class TestUser(TestCase):
+    def setUp(self):
+        self.user_credentials = {
+            'username': 'login_user',
+            'password': 'lup@ssW@rd'
+        }
+        self.user = User.objects.create_user(**self.user_credentials)
+
     def test_user_login_url(self):
         response = self.client.get(reverse_lazy('login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='user/user_form.html')
 
-    def test_user_login(self):
-        
+
+    def test_login(self):
+        response = self.client.post(reverse_lazy('login'), data=self.user_credentials, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse_lazy('home'))
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_logout(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('home'))
+        self.assertContains(response, 'logout')
+
+        response = self.client.post(reverse_lazy('logout'), True)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'logout')
