4408da7bace268a8229e499f872633c2149f6401
midnight <ivanator666@gmail.com>
2024-03-12 22:50:50 +0300

    updated status test

--- task_manager/tests/test_status.py
+++ task_manager/tests/test_status.py
@@ -4,7 +4,15 @@ from task_manager.user.models import User
 from task_manager.status.models import Status
 
 
-class TestStatus(TestCase):
+class TestStatusIndx(TestCase):
+
+    def test_status_page(self):
+        response = self.client.get(reverse_lazy('status_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTempateUsed(response, tempate_name='status/statuses.html')
+
+
+class TestStatusCRUD(TestCase):
     def setUp(self):
         self.user_credentials = {
             'username': 'login_user',
@@ -13,10 +21,29 @@ class TestStatus(TestCase):
         self.user = User.objects.create_user(**self.user_credentials)
         self.client.force_login(self.user)
 
-    def test_status_page(self):
-        response = self.client.get(reverse_lazy('status_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTempateUsed(response, tempate_name='status/statuses.html')
+        self.status_data = {'name': '1st'}
 
-    def test_status_creation(self):
-        pass
+    def test_status_creation_deletion(self):
+        response = self.client.post(
+                reverse_lazy('status_create'),
+                data=self.status_data
+                )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('statuses'))
+
+        response = self.client.post(
+                reverse_lazy('status_delete', kwargs={'pk': 1})
+                )
+
+        self.assertRedirects(response, reverse_lazy('statuses'))
+        self.assertEqual(len(Status.objects.all()), 0)
+
+    def test_status_update(self):
+        Status.objects.create(**self.status_data)
+        response = self.client.post(
+                reverse_lazy('status_update', kwargs={'pk': 1}),
+                data={'name': 'updated'},
+                follow=True
+                )
+
+        self.assertContains(response, 'updated')
