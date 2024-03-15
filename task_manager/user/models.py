80232ceed6e295ee6ab23fc2d6b15d73d1967ac9
Midnight <ivanator666@gmail.com>
2024-03-15 12:56:25 +0300

    updated user model

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100, null=True, unique=True)
+    username = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
