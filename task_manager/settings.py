ee2d7b28fea8fc74389fe666c24d3946886a6ed9
midnight <ivanator666@gmail.com>
2024-05-07 00:39:31 +0300

    altered settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -108,12 +108,11 @@ if DATABASE_URL:
     }
 else:
     DATABASES = {
-        'default':
-            {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-                }
-            }
+        'default': dj_database_url.config(
+            default='sqlite:///db.sqlite3',
+            conn_max_age=600
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
