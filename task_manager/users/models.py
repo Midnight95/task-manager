a21c3eff5feea50f7ae7b49fe688b9d84593978d
Midnight <ivanator666@gmail.com>
2024-05-08 09:22:54 +0300

    mergred changes

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,19 +4,6 @@ from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
-    username = models.CharField(
-        max_length=100,
-        unique=True,
-        verbose_name=_('Username')
-    )
-    first_name = models.CharField(
-        max_length=100,
-        verbose_name=_('First name')
-        )
-    last_name = models.CharField(
-        max_length=100,
-        verbose_name=_('Last name')
-    )
 
     def __str__(self):
         return self.username
