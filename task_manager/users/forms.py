8ecff02f80d439f2e9e4808d3aeb61dd79739189
Midnight <ivanator666@gmail.com>
2024-05-08 09:10:38 +0300

    updated forms

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -4,15 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
 
     class Meta:
         model = User
         fields = [
+                'username',
                 'first_name',
                 'last_name',
-                'username',
                 'password1',
                 'password2'
                 ]
